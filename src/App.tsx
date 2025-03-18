5f601246be03cc3623764206a0b95b162078baa1
Kvai <htckvai@gmail.com>
2025-03-18 12:28:42 +0300

    task1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 
-import './App.css'
+import "./App.css";
+import { useFetch } from "./useFetch";
 
 function App() {
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+  const { data, isLoading, error, refetch } = useFetch(url);
 
   return (
     <>
-        <div>
-        </div>
+      <h1>useFetch</h1>
+      <div className="card">
+        <button
+          onClick={() =>
+            refetch({
+              params: {
+                _limit: 3,
+              },
+            })
+          }
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading..." : 'Перезапросить'}
+        </button>
+       
+          <p>{isLoading && "Загрузка..."}</p>
+          <p>{error && "Произошла ошибка"}</p>
+        
+        {data && !isLoading && data.map(item => <div key={item.id}>{item.title}</div>) }
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
