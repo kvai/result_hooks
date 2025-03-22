ff7f33570829e3ca5ca9e1c13062234958190bce
Kvai <htckvai@gmail.com>
2025-03-22 11:05:38 +0300

    handleChange and handleSubmit added to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-
-import './App.css'
+import {  useState } from "react";
+import "./App.css";
+import { Signin } from "./components/Signin";
+import { Signup } from "./components/Signup";
 
 function App() {
+  const initialState = {};
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (event: React.ChangeEvent<HTMLFormElement>) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("Отправленные данные: ", values);
+    setValues(initialState);
+  };
 
   return (
     <>
-        <div>
-        </div>
+      <Signin handleSubmit={handleSubmit} handleChange={handleChange} />
+      <Signup handleSubmit={handleSubmit} handleChange={handleChange} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
