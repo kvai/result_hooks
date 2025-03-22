cc6628a90b3cee37c0e820bbd328afe0ee7d5927
Kvai <htckvai@gmail.com>
2025-03-22 11:04:32 +0300

    Signup component added

---
+++ src/components/Signup.tsx
@@ -0,0 +1,49 @@
+
+import { CombinedProps } from "../types";
+import { Input } from "./input/Input";
+
+export const Signup = ({ handleSubmit, handleChange, }: CombinedProps) => {
+
+  return (
+    <form onSubmit={handleSubmit} onChange={handleChange}>
+      <Input
+        label="Имя"
+        placeholder="Введите имя"
+        withAsterisk
+        name="name"
+        radius="xl"
+        error="Some error"
+      />
+      <Input
+        label="Ник"
+        placeholder="Введите Ник"
+        withAsterisk
+        name="nick"
+        variant="filled"
+      />
+      <Input
+        label="Почта"
+        placeholder="Введите Почту"
+        type="email"
+        name="email"
+      />
+      <Input type="radio" name="sex" value="мужской" label="мужской" />
+      <Input type="radio" name="sex" value="женский" label="женский" />
+      <Input
+        label="Пароль"
+        placeholder="Введите Пароль"
+        type="password"
+        withAsterisk
+        name="password"
+      />
+      <Input
+        label="Повторите пароль"
+        placeholder="Повторите пароль"
+        type="password"
+        withAsterisk
+        name="confirmPassword"
+      />
+      <button type="submit">Войти</button>
+    </form>
+  );
+};
