1fe982d6906f3a586e35cccf7132236dae39c611
Kvai <htckvai@gmail.com>
2025-04-10 19:42:16 +0300

    Header component added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { Navbar } from "../Navbar/Navbar";
+import logo from "../../../assets/images/logo.svg";
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <Link to={'/'}>
+            <img style={{ width: 600 }} src={logo} alt="Logo" />
+          </Link>
+        </div>
+        <Navbar />
+      </div>
+    </>
+  );
+};
