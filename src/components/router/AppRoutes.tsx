991bffb4072e5b42d7c4f1d993cf3db3ca94e488
Kvai <htckvai@gmail.com>
2025-04-14 18:03:12 +0300

    Added PrivateRoute component

--- src/components/router/AppRoutes.tsx
+++ src/components/router/AppRoutes.tsx
@@ -4,30 +4,37 @@ import { MainLayout } from "../MainLayout";
 import { Card } from "../Card/Card";
 import { NotFound } from "../NotFound";
 import { ItemsList } from "../ItemsList/ItemsList";
+import { Login } from "../Login";
+import { PrivateRoute } from "../PrivateRoute";
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route index element={<Home />} />
 
-        <Route path="characters">
-          <Route index element={<ItemsList dataType="characters" />} />
-          <Route path=":id" element={<Card dataType="characters" />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="characters">
+              <Route index element={<ItemsList dataType="characters" />} />
+              <Route path=":id" element={<Card dataType="characters" />} />
+            </Route>
+
+            <Route path="episodes">
+              <Route index element={<ItemsList dataType="episodes" />} />
+              <Route path=":id" element={<Card dataType="episodes" />} />
+            </Route>
+
+            <Route path="locations">
+              <Route index element={<ItemsList dataType="locations" />} />
+              <Route path=":id" element={<Card dataType="locations" />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
         </Route>
-
-        <Route path="episodes">
-          <Route index element={<ItemsList dataType="episodes" />} />
-          <Route path=":id" element={<Card dataType="episodes" />} />
-        </Route>
-
-        <Route path="locations">
-          <Route index element={<ItemsList dataType="locations" />} />
-          <Route path=":id" element={<Card dataType="locations" />} />
-        </Route>
-
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </>
   );
 };
