e384b3880c69835ea8c83e8caa21874b4780e937
Kvai <htckvai@gmail.com>
2025-04-14 17:54:53 +0300

    Moved types

---
+++ types/types.ts
@@ -0,0 +1,35 @@
+export type ResultType = Character[] | Episode[] | Location[];
+
+export interface DataType {
+  dataType: "characters" | "locations" | "episodes";
+}
+
+interface BaseCard {
+  id: number;
+  name: string;
+  created: string;
+}
+
+interface CharactersCard extends BaseCard {
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  image: string;
+}
+
+interface EpisodesCard extends BaseCard {
+  air_date: string;
+  episode: string;
+}
+
+interface LocationCard extends BaseCard {
+  type: string;
+  dimension: string;
+}
+
+export type Card = CharactersCard | EpisodesCard | LocationCard;
+
+export interface ApiResponse {
+  isLoading: boolean;
+}
