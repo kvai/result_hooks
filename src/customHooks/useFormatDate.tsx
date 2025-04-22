9891d8383f0ccc90856d18aff1ebee4db12e6da2
Kvai <htckvai@gmail.com>
2025-04-22 22:48:32 +0300

    refactor: update useFormatDate component

--- src/customHooks/useFormatDate.tsx
+++ src/customHooks/useFormatDate.tsx
@@ -1,6 +1,6 @@
 export const useFormatDate = (dateString: Date) => {
   const date = new Date(dateString);
-  const formattedDate = new Intl.DateTimeFormat("ru-RU",).format(date);
+  const formattedDate = new Intl.DateTimeFormat("ru-RU").format(new Date(date));
 
   return formattedDate;
 };
