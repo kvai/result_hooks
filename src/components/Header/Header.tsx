15cfc837ff65ae059fdee03296088dae20190159
Kvai <htckvai@gmail.com>
2025-04-29 22:00:53 +0300

    feat: add grid to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,21 @@ import { Navbar } from "../Navbar/Navbar";
 import logo from "../../../assets/images/logo.svg";
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
+import { Grid } from "@mantine/core";
 
 export const Header = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.logo}>
-        <Link to={"/"}>
-          <img style={{ maxWidth: "100%" }} src={logo} alt="Logo" />
-        </Link>
-      </div>
-      <Navbar />
-    </div>
+    <Grid justify={"space-between"}>
+      <Grid.Col span="content">
+        <div className={styles.logo}>
+          <Link to={"/"}>
+            <img style={{ maxWidth: "100%" }} src={logo} alt="Logo" />
+          </Link>
+        </div>
+      </Grid.Col>
+      <Grid.Col span="content">
+        <Navbar />
+      </Grid.Col>
+    </Grid>
   );
 };
